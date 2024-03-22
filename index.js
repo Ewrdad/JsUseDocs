d82e440569a05d3df63384660adfad9ab405b382
Ewrdad <60289100+Ewrdad@users.noreply.github.com>
2024-03-22 13:29:57 +0000

    Added loose wireframe for package

---
+++ index.js
@@ -0,0 +1,22 @@
+
+/**
+ * @title JsUseDocs-Core
+ * @version 1.0.0
+ * @description JsUseDocs-Core is a core library for JsUseDocs in js
+ * @returns JsUseDocsCore functions
+ */
+export const JsUseDocsCore = () =>{ 
+
+return {
+    parseFlat : "Not implemented yet",
+    parseFlatSummary : "Not implemented yet",
+    parseTags : "Not implemented yet",
+    isDoc : "Not implemented yet",
+    includesdoc : "Not implemented yet",
+    addTag : "Not implemented yet",
+    removeTag : "Not implemented yet",
+    setTags : "Not implemented yet",
+    parseNested : "Not implemented yet",
+    parseNestedSummary : "Not implemented yet"
+}
+}
